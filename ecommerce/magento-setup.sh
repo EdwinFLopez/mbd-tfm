fd84f2c92f971974660578f603345ec13d484928
Edwin F. Lopez <edwinflopez@users.noreply.github.com>
2024-09-02 12:43:22 +0200

    setting oneliner instead of template

--- ecommerce/magento-setup.sh
+++ ecommerce/magento-setup.sh
@@ -5,5 +5,5 @@ DOMAIN=${1:-magento.test}
 VERSION=${2:-2.4.7}
 EDITION=${3:-community}
 
-echo "Installing: Magento $DOMAIN $VERSION $EDITION"
-curl -s https://raw.githubusercontent.com/edwinflopez/docker-magento/master/lib/template | bash -s -- "$DOMAIN" "$VERSION" "$EDITION"
+echo "Installing: Magento @$DOMAIN $EDITION v$VERSION"
+curl -s https://raw.githubusercontent.com/edwinflopez/docker-magento/master/lib/onelinesetup | bash -s -- "$DOMAIN" "$VERSION" "$EDITION"
