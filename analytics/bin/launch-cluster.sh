cbbbb834243a25c81b9a2b8f35925a5a589d3ed5
Edwin F <10159067+edwinflopez@users.noreply.github.com>
2025-02-08 20:44:44 +0100

    configuring more parameters for the spark cluster parameters file, updating launch script

--- analytics/bin/launch-cluster.sh
+++ analytics/bin/launch-cluster.sh
@@ -2,28 +2,24 @@
 set -eo pipefail
 
 # Prepare Spark Connect vars and command
-SPARK_CONNECT_PACKAGES="$GROUP:$ARTIFACT:$VERSION"
-IVY_OPTS="-Divy.home=$SPARK_HOME/tmp -Divy.cache.dir=$SPARK_HOME/tmp"
-SPARK_CONNECT_JAVAOPT="spark.driver.extraJavaOptions='$IVY_OPTS'"
-LOGS_DIR="/opt/spark/work-dir/data/logs"
 export PATH="$SPARK_HOME/sbin:$PATH"
 
 # Launch Master Node
-#sh -c "start-master.sh" > $LOGS_DIR/spark-master.log 2>&1 &
+#sh -c "start-master.sh" > $SPARK_LOG_DIR/spark-master.log 2>&1 &
 sh -c "start-master.sh" &
 
 # Launch Worker Node
-#sh -c "start-worker.sh spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT" > $LOGS_DIR/spark-worker.log 2>&1 &
+#sh -c "start-worker.sh spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT" > $SPARK_LOG_DIR/spark-worker.log 2>&1 &
 sh -c "start-worker.sh spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT" &
 
 # Launch History Node
-#sh -c "start-history-server.sh" > $LOGS_DIR/spark-history.log 2>&1 &
+#sh -c "start-history-server.sh" > $SPARK_LOG_DIR/spark-history.log 2>&1 &
 sh -c "start-history-server.sh" &
 
 # Launch Spark Connect
-#sh -c "start-connect-server.sh --conf '$SPARK_CONNECT_JAVAOPT' --packages $SPARK_CONNECT_PACKAGES" > $LOGS_DIR/spark-connect.log 2>&1 &
-sh -c "start-connect-server.sh --conf '$SPARK_CONNECT_JAVAOPT' --packages $SPARK_CONNECT_PACKAGES" &
+#sh -c "start-connect-server.sh --conf '$SPARK_CONNECT_JAVAOPTS' --packages '$SPARK_CONNECT_PKGS'" > $SPARK_LOG_DIR/spark-connect.log 2>&1 &
+sh -c "start-connect-server.sh --conf '$SPARK_CONNECT_JAVAOPTS' --packages '$SPARK_CONNECT_PKGS'" &
 
-#sh -c "tail -f $LOGS_DIR/spark-master.log $LOGS_DIR/spark-worker.log $LOGS_DIR/spark-connect.log $LOGS_DIR/spark-history.log"
+#sh -c "tail -f $SPARK_LOG_DIR/spark-master.log $SPARK_LOG_DIR/spark-worker.log $SPARK_LOG_DIR/spark-connect.log $SPARK_LOG_DIR/spark-history.log"
 echo "Spark Services Up"
 wait
