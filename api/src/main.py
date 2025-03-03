10b816c15f8433b6a1bff5adfdb9587021516c1a
Edwin F <10159067+edwinflopez@users.noreply.github.com>
2025-03-03 11:26:20 +0100

    updating architecture diagram and spark connect url

--- api/src/main.py
+++ api/src/main.py
@@ -4,11 +4,10 @@ from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
     sc_url = "sc://localhost:15002"
-    session = SparkSession.builder \
-            .appName("MBDTFM") \
-            .remote(sc_url) \
-            .config("spark.mongodb.read.connection.uri", "mongodb://localhost:27017/mbdtfmdb?directConnection=true") \
-            .config("spark.mongodb.write.connection.uri", "mongodb://localhost:27017/mbdtfmdb?directConnection=true") \
+    mongo_url = "mongodb://localhost:27017/mbdtfmdb?directConnection=true"
+    session = SparkSession.builder.appName("MBDTFM").remote(sc_url) \
+            .config("spark.mongodb.read.connection.uri", mongo_url) \
+            .config("spark.mongodb.write.connection.uri", mongo_url) \
             .getOrCreate()
     session.read.format("mongo").load()
     type(session)
