12df56499264442bfc93d74bc4b823ce9f0bf90a
Edwin F. Lopez <10159067+edwinflopez@users.noreply.github.com>
2025-06-22 12:05:56 +0200

    Updating the query for m_view

--- temp.sql
+++ temp.sql
@@ -30,8 +30,7 @@ with category_entity as (
            pas.attribute_set_id,
            pas.attribute_set_name
     from catalog_product_entity cpe
-             join product_attributeset pas
-                  on cpe.attribute_set_id = pas.attribute_set_id
+             join product_attributeset pas using (attribute_set_id)
 ), product_attributes as (
     select eea.attribute_set_id,
            eea.attribute_id,
@@ -77,40 +76,90 @@ with category_entity as (
         from product_attributeset pas
         where eea.attribute_set_id = pas.attribute_set_id
     )
+), products_with_attributes as (
+    select prd.catalog_product_id,
+           prd.sku,
+           prd.type_id,
+           prd.attribute_set_id,
+           prd.attribute_set_name,
+           attr.attribute_id,
+           attr.attribute_code,
+           attr.backend_type,
+           attr.default_value,
+           attr.attribute_label,
+           attr.frontend_input_renderer,
+           attr.is_global,
+           attr.is_visible,
+           attr.is_searchable,
+           attr.is_filterable,
+           attr.is_comparable,
+           attr.is_visible_on_front,
+           attr.is_html_allowed_on_front,
+           attr.is_used_for_price_rules,
+           attr.is_filterable_in_search,
+           attr.used_in_product_listing,
+           attr.used_for_sort_by,
+           attr.apply_to,
+           attr.is_visible_in_advanced_search,
+           attr.position,
+           attr.is_wysiwyg_enabled,
+           attr.is_used_for_promo_rules,
+           attr.is_required_in_admin_store,
+           attr.is_used_in_grid,
+           attr.is_visible_in_grid,
+           attr.is_filterable_in_grid,
+           attr.search_weight,
+           attr.is_pagebuilder_enabled,
+           attr.additional_data
+    from product_attributes attr
+             join catalog_products prd using (attribute_set_id)
 )
-select *
-from catalog_products;
-
-
-
-
-select eea.attribute_set_id, eea.attribute_id,
-       eat.backend_type,
-       eat.default_value,
-       eat.attribute_code,
-       eat.frontend_label as attribute_label,
-       cea.*
-from eav_entity_attribute eea
-join eav_attribute eat
-    on eea.attribute_id = eat.attribute_id
-        and eat.entity_type_id = 4
-        and eea.attribute_set_id = 15
-join magento.catalog_eav_attribute cea
-    on eat.attribute_id = cea.attribute_id
--- where
--- cea.is_global = 1 and
--- cea.is_visible = 1
--- or
--- cea.is_visible_on_front = 1
--- or
--- cea.is_searchable = 1
--- or
--- cea.is_filterable_in_search = 1
--- or
--- cea.is_visible_in_advanced_search = 1
--- or
--- cea.is_filterable_in_grid = 1
+select pwa.catalog_product_id,
+       pwa.sku,
+       pwa.type_id,
+       pwa.attribute_set_id,
+       pwa.attribute_set_name,
+       pwa.attribute_id,
+       pwa.attribute_code,
+       pwa.backend_type,
+       pwa.default_value,
+       pwa.attribute_label,
+       pwa.is_visible,
+       pwa.is_searchable,
+       pwa.is_filterable,
+       pwa.apply_to,
+       pwa.additional_data,
+       txt.store_id,
+       txt.value,
+       var.store_id as var_store_id,
+       var.value as var_value,
+       vdt.store_id as vdt_store_id,
+       vdt.value as vdt_value,
+       vdc.store_id as vdc_store,
+       vdc.value as vdc_value,
+       vin.store_id as vin_store_id,
+       vin.value as vin_value
+from products_with_attributes pwa
+         left join catalog_product_entity_text txt
+                   on  pwa.catalog_product_id = txt.entity_id
+                       and pwa.attribute_id = txt.attribute_id
+         left join catalog_product_entity_varchar var
+                   on  pwa.catalog_product_id = var.entity_id
+                       and pwa.attribute_id = var.attribute_id
+         left join catalog_product_entity_datetime vdt
+                   on  pwa.catalog_product_id = vdt.entity_id
+                       and pwa.attribute_id = vdt.attribute_id
+         left join catalog_product_entity_decimal vdc
+                   on  pwa.catalog_product_id = vdc.entity_id
+                       and pwa.attribute_id = vdc.attribute_id
+         left join catalog_product_entity_int vin
+                   on  pwa.catalog_product_id = vin.entity_id
+                       and pwa.attribute_id = vin.attribute_id
+where pwa.sku like '24-MB06%'
+-- where pwa.sku = '24-MB01'
 ;
+
+
 
 SELECT t_d.attribute_id,
        e.entity_id,
