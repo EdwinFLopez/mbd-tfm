aa15eaeb8d690a2e7b75e6386c6a7a21eaad3690
Edwin F. Lopez <10159067+edwinflopez@users.noreply.github.com>
2025-09-19 10:35:56 +0200

    Adding docker container initialization to webapi and webapp

--- api/src/commons/constants.py
+++ api/src/commons/constants.py
@@ -10,7 +10,7 @@ SPARK_CONNECT_URL = f"sc://{SPARK_CONNECT_HOST}:{SPARK_CONNECT_PORT}"
 # ###############################################################
 # MongoDb required environment variables and default values
 MONGO_HOST = os.getenv("MONGO_HOST", "mongodb-atlas")
-MONGO_PORT = int(os.getenv("MONGO_HOST", 27017))
+MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
 MONGO_URL = f"mongodb://{MONGO_HOST}:{MONGO_PORT}"
 # ###############################################################
 
